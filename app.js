5e5e54cc81ea15e60dbab5edc724d77b7f86d693
PRINCELEY <onuprinceley@gmail.com>
2023-04-18 01:53:23 +0100

    updated code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const axios=require("axios");
+//const axios=require("axios");
 const https=require("https");
 const express= require("express");
 const bodyParser=require("body-parser");
@@ -16,14 +16,14 @@ app.get('/',
    try {
     const query=req.body.cityName
     const unit="metric"
-    const apiKey='6d07de673c16aa09bdcd24a4c11b4d91';
+    const apiKey=process.env.Api_Key;
     const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units=metric"
     await https.get(url, function(response){
       response.on("data", function(data){
     
         const answer=JSON.parse(data);
         const desc=answer.weather[0].description;
-        const icon=answer.weather[0].icon
+        const icon=answer.weather[0].icon;
         const temp=answer.main.temp;
         const iconUrl=` https://openweathermap.org/img/wn/${icon}@2x.png`
         res.write("<img src="+iconUrl+" alt='photoImg'> ")
